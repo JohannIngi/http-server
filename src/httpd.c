e9762469fb3f500ff09329d4995f00a3eab5af5b
Johann Ingi <johannb16@ru.is>
2017-10-08 19:04:37 +0000

    multiple clients implemented, keep alive does not work

--- src/httpd.c
+++ src/httpd.c
@@ -1,6 +1,7 @@
 
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/poll.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
@@ -12,6 +13,7 @@
 #include <arpa/inet.h>
 
 #define QUEUED 5
+#define MAX_CLIENTS 10
 
 // TODO: fake_job is just to check if some function need handling to remove warnings - remove at end product
 void fake_job() {
@@ -23,15 +25,14 @@ void debug(const char* msg) {
     putchar('\n');
 }
 
-
 typedef struct sockaddr_in sockaddr_in;
 typedef struct sockaddr sockaddr;
+typedef struct pollfd pollfd;
 
 typedef struct 
 {
     sockaddr_in address;
     socklen_t len;
-    //char data[2048];
     char time_buffer[20];
     char header_buffer[1024];
     char url_buffer[1024];
@@ -39,10 +40,14 @@ typedef struct
     char* ip_address;
     unsigned short port;
 } client_info;
+
 typedef struct
 {
     int sockfd;
+    int timeout;
     sockaddr_in address;
+    //making an array of pollfd's to handle multiple clients
+    pollfd fds[MAX_CLIENTS];
     char buffer[4096];
     char url_for_all[1024];
     char data_buffer[2048];
@@ -50,10 +55,11 @@ typedef struct
 }server_info;
 
 void startup_server(server_info* server, const char* port);
-void run_server(server_info* server, client_info* client);
+void setup_multiple_clients(server_info* server);
+void run_server(server_info* server, client_info* clients);
+int find_next_available_client(pollfd* fds);
 int accept_request(client_info* client, server_info* server);
 void get_client_information(client_info* client);
-void receive_message(server_info* server, int* connfd);
 void get_header_type(server_info* server, client_info* client);
 void get_time(client_info* client);
 void generate_html_header(client_info* client, int response_code);
@@ -63,8 +69,9 @@ void generate_404_error(client_info* client);
 void write_to_log(client_info* client);
 void get_url(server_info* server);
 void get_post_data(server_info* server);
-void client_handle(server_info* server, client_info* client, int* connfd);
+void client_handle(server_info* server, client_info* client, int connfd);
 void error_handler(char* error_buffer);
+
 
 /*********************************************************************************************************/
 /*  main function starts here and declares instances of the structs that are used thougout the server    */
@@ -74,11 +81,10 @@ int main(int argc, char **argv)
     if (argc != 2) error_handler("invalid arguments");
     char* welcome_port = argv[1];
     server_info server;
-    client_info client;
+    client_info clients[MAX_CLIENTS];
     fprintf(stdout, "Listening to server number: %s\n", welcome_port); fflush(stdout);  
     startup_server(&server, welcome_port);
-    run_server(&server, &client);
-
+    run_server(&server, clients);
 }
 
 /*************************************************************************/
@@ -101,36 +107,107 @@ void startup_server(server_info* server, const char* port){
     if(listen(server->sockfd, QUEUED) < 0){error_handler("listen failed");}
 }
 /*************************************************************************/
+/*  Setting up multiple clients connections                              */
+/*************************************************************************/
+void setup_multiple_clients(server_info* server){
+
+    memset(&server->fds, 0, sizeof(pollfd));
+    for(int i = 0; i < MAX_CLIENTS; i++){
+        server->fds[i].fd = -1; //initializing all the objects in the array to -1, we only handle cases where the object is 1
+        server->fds[i].events = POLLIN;
+    }
+    server->fds[0].fd = server->sockfd; //giving the first object in the array the value of the file descriptor in poll()
+
+    server->timeout = 3 * 1000; //time duration for timeout set to 3 sec   
+}
+/*************************************************************************/
 /*  Running the server loop and calling necessary functions              */
 /*************************************************************************/
-void run_server(server_info* server, client_info* client){
+void run_server(server_info* server, client_info* clients){
 
+    setup_multiple_clients(server);
     fprintf(stdout, "Socket set up complete. Listening for request.\n"); fflush(stdout);
     
-    while(1) {
+    while(23) {
+        // saving the outcome in a variable
+        int rc = poll(server->fds, MAX_CLIENTS, server->timeout);//int poll(struct pollfd *fds, nfds_t nfds, int timeout);
+        if(rc < 0){
+            error_handler("something went wrong!");
+        }
+        else if(rc == 0){
+            debug("Nothing has happened for 3 sec");
+        } else{
+            debug("!!!!!!something is in my client!!!!!!");
 
-        fprintf(stdout, "Waiting to receive message...\n"); fflush(stdout);
-        int connfd = accept_request(client, server);
+            if(server->fds[0].revents & POLLIN){ //our new client is here
+                debug("!!!NEW client baby!!!");
+                //TODO error handle
+                int next_client = find_next_available_client(server->fds); //iterates through all the fd's until he returns a clien
+                fprintf(stdout, "NEXT CLIENT %d\n", next_client); fflush(stdout);
+                //server->fds[next_client].events = POLLIN;
 
-        get_client_information(client);
-        //fprintf(stdout, "From ip address: %s. Port number: %hu\n", client.ip_address, client.port); fflush(stdout);
+                //setting the buffer to zero
+                memset(&clients[next_client].address, 0, sizeof(sockaddr_in));
+                clients[next_client].len = (socklen_t) sizeof(sockaddr_in);
 
-        receive_message(server, &connfd);
+                int connfd = accept(server->fds[0].fd, (sockaddr *) &clients[next_client].address, &clients[next_client].len);
 
-        get_header_type(server, client);
-        get_url(server);
+                //if -1 then the client is inactive
+                if (connfd < 0){error_handler("accepting message failed");}
+                server->fds[next_client].fd = connfd; // give the client the fd value that
 
-        fprintf(stdout, "HEADER TYPE: %s\n", client->header_buffer);fflush(stdout);
-        fprintf(stdout, "Received message:\n\n--------------------\n%s\n", server->buffer);fflush(stdout);
+                //server->fds[0].events = 0; //ekki viss með þetta
+            }
 
-        client_handle(server, client, &connfd);
-        fprintf(stdout, "URL...:%s\n", client->url_buffer); fflush(stdout);
-        write_to_log(client);
+            for(int i = 1; i < MAX_CLIENTS; i++){
+
+                if(server->fds[i].fd != -1 && server->fds[i].revents & POLLIN){
+                    debug("!!!same OLD client!!!");
+                    //setting the buffer to zero
+                    memset(server->buffer, 0, sizeof(server->buffer));
+                    // Receive from clients fd connfd, not sockfd.
+                    ssize_t recv_msg_len = recv(server->fds[i].fd, server->buffer, sizeof(server->buffer)-1, 0);
+                    fprintf(stdout, "receive msg len: %zu\n", recv_msg_len);
+                    server->buffer[recv_msg_len] = '\0';
+                    fprintf(stdout, "Received message from client in array pos:  %d:\n\n--------------------\n%s\n", i, server->buffer);fflush(stdout);
+
+
+                    //sending in information about the specific client to be handled
+                    client_handle(server, &clients[i], server->fds[i].fd);
+                    write_to_log(&clients[i]);
+
+                    server->fds[i].events = POLLIN;
+
+                    close(server->fds[i].fd);
+                    server->fds[i].fd = -1;
+                }
+            }
+        }
+
+        /*
 
         // Close the connection.
         shutdown(connfd, SHUT_RDWR);
         close(connfd);
+        */
+
+
+        // TODO: check for timed out clients...
     }    
+}
+
+/****************************************************/
+/*  Finding the next available client               */
+/****************************************************/
+int find_next_available_client(pollfd* fds){
+
+    int i;
+    for(i = 1; i < MAX_CLIENTS; i++){
+        if(fds[i].fd == -1){
+            return i; //skila staðsetningunni sem er með laust pláss
+        }
+    }
+    return -1;
 }
 /*************************************/
 /*  Accepting a request from client  */
@@ -153,16 +230,7 @@ void get_client_information(client_info* client){
     //storing information about the port from client
     client->port = client->address.sin_port;
 }
-/*************************************/
-/*  Receiving message from someone   */
-/*************************************/
-void receive_message(server_info* server, int* connfd){
-    //setting the buffer to zero
-    memset(server->buffer, 0, sizeof(server->buffer));
-    // Receive from connfd, not sockfd.
-    ssize_t recv_msg_len = recv(*connfd, server->buffer, sizeof(server->buffer) - 1, 0);
-    server->buffer[recv_msg_len] = '\0';
-}
+
 //*******************************************************************************/
 //  Getting the header type from the clients message. Is it a get, post or head */
 //*******************************************************************************/
@@ -223,6 +291,7 @@ void generate_html_header(client_info* client, int response_code){
 /*  Hard-coded html for the get request  */
 /*****************************************/
 void generate_html_get(client_info* client){
+
     generate_html_header(client, 200);
     //appending all the html code that is needed to generate a page
     strcat(client->html, "<!DOCTYPE html>\r\n<html><head><title>HTTPServer</title></head>\r\n");
@@ -249,6 +318,7 @@ void generate_html_get(client_info* client){
 /*  Hard-coded html for the post request     */
 /*********************************************/
 void generate_html_post(client_info* client, server_info* server){
+
     generate_html_header(client, 200);
     //appending all the html code that is needed to generate a page
     strcat(client->html, "<!DOCTYPE html>\r\n<html><head><title>HTTPServer</title></head>\r\n");
@@ -267,10 +337,11 @@ void generate_html_post(client_info* client, server_info* server){
     strcat(client->html, port_fix);
     //adding a form to receive a post request
     strcat(client->html, "<form method=\"post\">\r\n<label for=\"name1\">Name1:</label>\r\n<input type=\"name\" name=\"name\">\r\n<label for=\"name2\">Name2:</label>\r\n<input type=\"name\" name=\"name\">\r\n<button>Submit</button>\r\n</form>");
-    strcat(client->html, "<table><tr><th>Data</th><th>something</th></tr><tr><td>");
+    strcat(client->html, "<center><table><tr><th>Data</th><th>something</th></tr><tr><td>");
     get_post_data(server);
+
     strcat(client->html, server->data_buffer);
-    strcat(client->html, "</td><td>something2</td></tr></table>");
+    strcat(client->html, "</td><td>something2</td></tr></table></center>");
     
     
     //closing the html page
@@ -280,6 +351,7 @@ void generate_html_post(client_info* client, server_info* server){
 /*  Writing to a log file in the root directory  */
 /*************************************************/
 void generate_404_error(client_info* client){
+
     generate_html_header(client, 404);
     strcat(client->html, "<!DOCTYPE html>\r\n<html><head><title>HTTPServer</title></head>\r\n");
     strcat(client->html, "<body>ERROR 404");
@@ -291,6 +363,7 @@ void generate_404_error(client_info* client){
 /*  Writing to a log file in the root directory  */
 /*************************************************/
 void write_to_log(client_info* client){
+
     get_time(client);
     //opening a file and logging a timestamp to it
     FILE* file;
@@ -338,7 +411,16 @@ void get_post_data(server_info* server){
 /*********************************************************************************************/
 /*  client handler that decides what to do based on the client request; get, post or head    */
 /*********************************************************************************************/
-void client_handle(server_info* server, client_info* client, int* connfd){
+void client_handle(server_info* server, client_info* client, int connfd){
+
+    get_client_information(client);
+    //fprintf(stdout, "From ip address: %s. Port number: %hu\n", client.ip_address, client.port); fflush(stdout);
+
+    get_header_type(server, client);
+    get_url(server);
+
+    fprintf(stdout, "HEADER TYPE: %s\n", client->header_buffer);fflush(stdout);
+
     //checking if the header_buffer matches a GET request
     if(g_str_match_string("GET", client->header_buffer, 1)){
         //copying the server url buffer from array index 5 into the client url buffer
@@ -347,17 +429,17 @@ void client_handle(server_info* server, client_info* client, int* connfd){
         if(server->url_for_all[5] == ' '){
             generate_html_get(client);
             //sending the page
-            send(*connfd, client->html, strlen(client->html), 0);
+            send(connfd, client->html, strlen(client->html), 0);
         }
         else if(strcmp(client->url_buffer, "favicon.ico")){
-            debug("RECIEVED A FAV ICON - SENDING THAT FAGGOT SOME ERROR - COCKKKKKKKKKKKKKKKKKKKKK");
+            debug("RECIEVED A FAV ICON - SENDING THAT FAGGOT SOME ERROR - COCK");
             generate_404_error(client);
-            send(*connfd, client->html, strlen(client->html), 0);
+            send(connfd, client->html, strlen(client->html), 0);
         }
         //sending 404 error
         else{
             generate_404_error(client);
-            send(*connfd, client->html, strlen(client->html), 0);
+            send(connfd, client->html, strlen(client->html), 0);
         }
     }
     
@@ -367,7 +449,7 @@ void client_handle(server_info* server, client_info* client, int* connfd){
         strcpy(client->url_buffer, &server->url_for_all[6]);
         generate_html_post(client, server);
         //fprintf(stdout, "DATA FOR POST: %s\n", server->data_buffer);fflush(stdout);
-        send(*connfd, client->html, strlen(client->html), 0);
+        send(connfd, client->html, strlen(client->html), 0);
  
     }
     //checking if the header_buffer matches a HEAD request
@@ -377,12 +459,14 @@ void client_handle(server_info* server, client_info* client, int* connfd){
         generate_html_header(client, 200);
         //closing the html page
         strcat(client->html, "\r\n</html>\r\n");
-        send(*connfd, client->html, strlen(client->html), 0);
+        send(connfd, client->html, strlen(client->html), 0);
 
     }
     else{error_handler("No match to header-type!");}
 }
-
+/*************************************************************************/
+/*  Write out the error message that are sent to this function           */
+/*************************************************************************/
 void error_handler(char* error_buffer){
     //printing error using the function perror()
     perror(error_buffer);
